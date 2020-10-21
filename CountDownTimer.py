d226b3c59379fb02579677390d1b52be7101e44c
MasthanSharifShaik <51940803+MasthanSharifShaik@users.noreply.github.com>
2020-10-21 16:28:13 +0530

    Added via Upload

---
+++ CountDownTimer.py
@@ -0,0 +1,39 @@
+# Program to count seconds in Python
+# import the time module 
+
+import time 
+
+  
+# define the countdown func. 
+
+def countdown(t): 
+
+    
+
+    while t: 
+
+        mins, secs = divmod(t, 60) 
+
+        timer = '⏳{:02d}:{:02d}'.format(mins, secs) 
+
+        print(timer, end="\r") 
+
+        time.sleep(1) 
+
+        t -= 1
+
+      
+
+    print("Time's Up! ⌛") 
+
+  
+
+  
+# input time in seconds 
+
+t = input("Enter time in seconds: ") 
+
+  
+# function call 
+
+countdown(int(t)) 
